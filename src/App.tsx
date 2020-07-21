047b996fcc66e9e95cfeda08ad6a364f382194b7
tmtakashi <tmtakashi7@gmail.com>
2020-07-22 07:44:55 +0900

    lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,55 @@
-import React, { useState } from 'react';
-import {
-  HashRouter,
-  withRouter,
-  Link,
-  Route,
-} from "react-router-dom";
-import { createMuiTheme } from '@material-ui/core/styles';
-import { ThemeProvider } from '@material-ui/core/styles';
+import React, { useState } from "react";
+import { HashRouter, withRouter, Link, Route } from "react-router-dom";
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/core/styles";
 
-import Home from './Screens/Home';
-import Production from './Screens/Production';
+import Home from "./Screens/Home";
+import Production from "./Screens/Production";
 
 const theme = createMuiTheme({
   palette: {
     primary: {
-      light: '#757ce8',
-      main: '#3f50b5',
-      dark: '#002884',
-      contrastText: '#fff',
+      light: "#757ce8",
+      main: "#3f50b5",
+      dark: "#002884",
+      contrastText: "#fff",
     },
     secondary: {
-      light: '#4aedc4',
-      main: '#1de9b6',
-      dark: '#14a37f',
-      contrastText: '#333',
+      light: "#4aedc4",
+      main: "#1de9b6",
+      dark: "#14a37f",
+      contrastText: "#333",
     },
   },
 });
 
 const Main = withRouter(({ location }) => {
-  const [participant, setParticipant] = useState('');
-  const [dataPath, setDataPath] = useState('');
+  const [participant, setParticipant] = useState("");
+  const [dataPath, setDataPath] = useState("");
   return (
     <div>
-      {location.pathname === '/' &&
-      <Home
-        participant={participant}
-        setParticipant={setParticipant}
-        setDataPath={setDataPath}
-      />
-      }
-      <Route path="/production">
-        <Production
-          dataPath={dataPath}
+      {location.pathname === "/" && (
+        <Home
+          participant={participant}
+          setParticipant={setParticipant}
+          setDataPath={setDataPath}
         />
+      )}
+      <Route path="/production">
+        <Production dataPath={dataPath} />
       </Route>
     </div>
-  )
+  );
 });
 
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <HashRouter>
-        <Main/>
+        <Main />
       </HashRouter>
     </ThemeProvider>
-  ) 
+  );
 };
 
 export default App;
--- src/Screens/Production.tsx
+++ src/Screens/Production.tsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import PlayerSlider from '../components/PlayerSlider';
-import Container from '@material-ui/core/Container';
-import getCombinations from '../utils/getCombinations';
-import shuffle from '../utils/shuffle';
-const { ipcRenderer } = require('electron');
+import React, { useState, useEffect } from "react";
+import PlayerSlider from "../components/PlayerSlider";
+import Container from "@material-ui/core/Container";
+import getCombinations from "../utils/getCombinations";
+import shuffle from "../utils/shuffle";
+const { ipcRenderer } = require("electron");
 
 interface Props {
   dataPath: string;
@@ -11,34 +11,43 @@ interface Props {
 
 const Production: React.FC<Props> = ({ dataPath }) => {
   const [filesSet, setFilesSet] = useState(false);
-  const [combinations, setCombinations] = useState([[]] as string[][])
+  const [combinations, setCombinations] = useState([[]] as string[][]);
   const [numWavCombination, setNumCombination] = useState(0);
   const [value, setValue] = useState(5);
   const [counter, setCounter] = useState(1);
   useEffect(() => {
-    ipcRenderer.send('request-production-wav-list');
-    ipcRenderer.on('production-wav-list-reply', (event: any, arg: any) => {
-      const cmb = shuffle(getCombinations(arg))
-      setCombinations(cmb)
-      setNumCombination(cmb.length)
+    ipcRenderer.send("request-production-wav-list");
+    ipcRenderer.on("production-wav-list-reply", (event: any, arg: any) => {
+      const cmb = shuffle(getCombinations(arg));
+      setCombinations(cmb);
+      setNumCombination(cmb.length);
       setFilesSet(true);
-    })
-  }, [])
+    });
+  }, []);
 
   const handleChange = (event: any, newValue: any) => {
     setValue(newValue);
   };
 
   return (
-      <>
-        { filesSet && (
-          <>
-            
-            <Container>
-              <div style={{ textAlign: 'center', marginTop: '50px', marginBottom: '70px', fontSize: '30px' }}>
-                <p>No. {counter} / {numWavCombination}</p>
-                You are selecting <span style={{ fontWeight: 'bold' }}>{value}</span>
-              </div>
+    <>
+      {filesSet && (
+        <>
+          <Container>
+            <div
+              style={{
+                textAlign: "center",
+                marginTop: "50px",
+                marginBottom: "70px",
+                fontSize: "30px",
+              }}
+            >
+              <p>
+                No. {counter} / {numWavCombination}
+              </p>
+              You are selecting{" "}
+              <span style={{ fontWeight: "bold" }}>{value}</span>
+            </div>
             <PlayerSlider
               counter={counter}
               setCounter={setCounter}
@@ -49,11 +58,11 @@ const Production: React.FC<Props> = ({ dataPath }) => {
               value={value}
               dataPath={dataPath}
             />
-            </Container>
-          </>
-        )}
-      </>
+          </Container>
+        </>
+      )}
+    </>
   );
-}
+};
 
 export default Production;
